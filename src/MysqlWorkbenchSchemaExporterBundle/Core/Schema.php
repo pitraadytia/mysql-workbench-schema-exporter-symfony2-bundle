9f85bb2321eed07bb7ecd0f635d5ea9cefc88769
jkober <josekober@yahoo.com.ar>
2014-09-12 09:57:17 -0300

    Se agrego comando para generar repositorios automaticamente con la version 2.1.5 del MysqlWorkBench export

--- src/MysqlWorkbenchSchemaExporterBundle/Core/Schema.php
+++ src/MysqlWorkbenchSchemaExporterBundle/Core/Schema.php
@@ -308,4 +308,25 @@ class Schema extends ContainerAware
 
         $bootstrap->postCompileModels($formatter, $document);
     }
+    public function exportWithRepository(OutputInterface $output)
+    {
+        $output->writeln(sprintf('Exporting "<info>%s</info>" schema', $this->getName()));
+
+        $outputEntityDir = $this->getOutpuEntitytDir();
+        $this->console->run(new ArrayInput(['command' => 'mkdir', 'path'=>$outputEntityDir]));
+        $bootstrap = new Bootstrap();
+
+        // define a formatter and do configuration
+        $formatter = $bootstrap->getFormatter($this->getOption('formatter'));
+        $formatter->setup($this->getFormatterParams());
+
+        // load document and export
+        $output->writeln(sprintf('Create Entities'));
+        $bootstrap->export(
+            $formatter,
+            $this->getMwbFile(),
+            $this->getOutpuModeltDir()
+        );        
+    }
+    
 }
